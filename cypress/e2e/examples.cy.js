c2f3f3413ad40239ccc626bb91fde1c43f7cf4e5
Abubakar Siddique <abz.siddique@gmail.com>
2023-11-26 17:15:24 +0000

    test: using fixtures and intercepts

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -19,4 +19,16 @@ describe('Various examples', () => {
     cy.getDataTest('nav-best-practices').click();
     cy.location('pathname').should("equal", "/best-practices");
   });
+  it.only('intercepts', () => {
+    cy.getDataTest('nav-examples').click();
+    cy.intercept('POST', 'http://localhost:3000/examples', {
+      /* 
+        ? fixture here is a file that we want to return as our response.
+        ? cypress creates /fixtures/example.json for us by default
+        ? use this to return mocked data or provide a literal body.message object
+      */  
+      fixture: 'example.json'
+    });
+    cy.getDataTest('post-btn').click();
+  })
 })
