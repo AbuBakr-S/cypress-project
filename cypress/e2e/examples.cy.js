563f9ebd2a76957b172ff4d0e76d7c5ed34681bb
Abubakar Siddique <abz.siddique@gmail.com>
2023-12-03 17:57:52 +0000

    test: add intercepts

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -1,5 +1,5 @@
 describe('Various examples', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => cy.visit('/examples'));
   it('multi-page testing', () => {
     cy.getDataTest('nav-overview').click();
     cy.location('pathname').should("equal", "/overview");
@@ -19,8 +19,7 @@ describe('Various examples', () => {
     cy.getDataTest('nav-best-practices').click();
     cy.location('pathname').should("equal", "/best-practices");
   });
-  it.only('intercepts', () => {
-    cy.getDataTest('nav-examples').click();
+  it('intercepts', () => {
     cy.intercept('POST', 'http://localhost:3000/examples', {
       /* 
         ? fixture here is a file that we want to return as our response.
@@ -30,5 +29,47 @@ describe('Various examples', () => {
       fixture: 'example.json'
     });
     cy.getDataTest('post-btn').click();
+  });
+  it.only('grudges', () => {
+    cy.getDataTest('grudge-list').within(() => {
+      cy.get('li').should('have.length', 0);
+    });
+    // clear button should not exists when no grudges have been set
+    cy.getDataTest('clear-btn').should('not.exist');
+    // check title
+    cy.getDataTest('grudge-list-title').should('have.text', 'Add Some Grudges');
+    cy.contains(/add some grudges/i);
+    // look for a nested input element
+    cy.getDataTest('grudge-input').within(() => {
+      cy.get('input').type('Smoking');
+    });
+    cy.getDataTest('add-grudge-btn').click();
+    cy.getDataTest('grudge-list').within(() => {
+      cy.get('li').should('have.length', 1);
+    });
+    // recheck title has updated after adding a grudge
+    cy.getDataTest('grudge-list-title').should('have.text', 'Grudges');
+    // add another grudge
+    cy.getDataTest('grudge-input').within(() => {
+      cy.get('input').type('Cyclists on the road');
+    });
+    cy.getDataTest('add-grudge-btn').click();
+    cy.getDataTest('grudge-list').within(() => {
+      cy.get('li').should('have.length', 2);
+      // cy.get('li') returns an array
+      cy.get('li').its(0).should('contains.text', 'Smoking');
+    });
+    // remove a grudge
+    cy.getDataTest('grudge-list').within(() => {
+      cy.get('li').its(0).within(() => {
+        cy.get('button').click();
+      });
+      cy.get('li').should('have.length', 1);
+    });
+    // look for the clear button implicitly
+    cy.getDataTest('clear-btn').click();
+    cy.getDataTest('grudge-list').within(() => {
+      cy.get('li').should('have.length', 0);
+    })
   })
 })
